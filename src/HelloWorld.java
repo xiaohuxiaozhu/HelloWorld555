7fdd54be12a81d44715bb5bae869a66e21c5d14b
xiaohuxiaozhu <polikon@hotmail.com>
2019-06-12 11:34:16 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+       public static void main(String[] args) {
+            System.out.println("Hello,World");
+            System.out.println("Today is July 19,2015. I am alive");
+
+
+       }
+}
+
